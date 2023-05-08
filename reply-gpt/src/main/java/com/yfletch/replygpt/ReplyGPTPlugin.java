161ffa5ad6d5bcf1ab37f03dfb6f6a258e425fbe
IAmAChildOfGod <130787196+IAmAChildOfGod@users.noreply.github.com>
2023-05-08 02:02:46 -0300

    changed the readme

--- reply-gpt/src/main/java/com/yfletch/replygpt/ReplyGPTPlugin.java
+++ reply-gpt/src/main/java/com/yfletch/replygpt/ReplyGPTPlugin.java
@@ -21,9 +21,9 @@ import org.pf4j.Extension;
 @Extension
 @Slf4j
 @PluginDescriptor(
-	name = "Reply GPT",
+	name = "Giant Mole",
 	enabledByDefault = false,
-	description = "Behold the power of AI"
+	description = "Giant Mole Plugin"
 )
 public class ReplyGPTPlugin extends Plugin
 {
@@ -33,84 +33,20 @@ public class ReplyGPTPlugin extends Plugin
 	@Inject
 	private ClientThread clientThread;
 
-	@Inject
-	private ReplyGPTConfig config;
-
-	@Inject
-	private ChatBot bot;
 
 	@Override
 	protected void startUp()
 	{
-		try
-		{
-			bot.init();
 
-			clientThread.invokeLater(() -> {
-				client.addChatMessage(
-					ChatMessageType.GAMEMESSAGE,
-					"Reply GPT",
-					"Reply GPT successfully activated.",
-					null
-				);
-			});
-		}
-		catch (RuntimeException e)
-		{
-			JOptionPane.showMessageDialog(client.getCanvas(), e.getMessage(), "", JOptionPane.ERROR_MESSAGE);
-		}
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		bot.forget();
+
 	}
 
-	@Subscribe
-	public void onChatMessage(ChatMessage event)
-	{
-		if (!config.enabled())
-		{
-			return;
-		}
-		log.info(event.toString()); //to remove
-		log.info(event.toString());
 
-		if (event.getType() == ChatMessageType.PRIVATECHAT
-			|| event.getType() == ChatMessageType.PUBLICCHAT)
-		{
-			Executors.newSingleThreadExecutor().execute(() -> {
-				var response = bot.respond(new Message(
-					event.getType() == ChatMessageType.PRIVATECHAT ? "private" : "public",
-					event.getName(),
-					event.getMessage()
-				));
-
-				if (response == null)
-				{
-					return;
-				}
-
-				System.out.println(event.getMessage());
-				System.out.println(response.getMessage());
-
-				if (event.getType() == ChatMessageType.PRIVATECHAT)
-				{
-					Keyboard.type((char) KeyEvent.VK_TAB);
-				}
-				else
-				{
-					Keyboard.sendEnter();
-				}
-
-				Time.sleep(2000);
-				Keyboard.type(response.getMessage());
-				Time.sleep(10000);
-//				Keyboard.sendEnter();
-			});
-		}
-	}
 
 	@Provides
 	ReplyGPTConfig provideConfig(ConfigManager configManager)
